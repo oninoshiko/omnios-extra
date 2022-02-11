b97f9e6df703f8af56f893315c618aaecf09ce30
A Hettinger <oninoshiko@gmail.com>
2022-02-10 19:28:11 -0600

    Add uwsgi

---
+++ build/uwsgi/build.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/bash
+#
+# {{{ CDDL HEADER
+#
+# This file and its contents are supplied under the terms of the
+# Common Development and Distribution License ("CDDL"), version 1.0.
+# You may only use this file in accordance with the terms of version
+# 1.0 of the CDDL.
+#
+# A full copy of the text of the CDDL should have accompanied this
+# source. A copy of the CDDL is also available via the Internet at
+# http://www.illumos.org/license/CDDL.
+# }}}
+#
+# Copyright 2022 A Hettinger (oninoshiko@gmail.com)
+
+. ../../lib/build.sh
+
+PROG=uwsgi
+VER=2.0.20
+PKG=ooce/server/uwsgi
+SUMMARY="fast, self-healing application container server"
+DESC="uwsgi - $SUMMARY"
+
+[ $RELVER -lt 151030 ] && exit 0
+
+#https://projects.unbit.it/downloads/uwsgi-2.0.20.tar.gz
+set_mirror = "https://projects.unbit.it/"
+set_arch 64
+
+init
+download_source "downloads" $PROG $VER
+prep_build
+build
+make_package
+clean_up
+
+# Vim hints
+# vim:ts=4:sw=4:et:fdm=marker
