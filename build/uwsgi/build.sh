bb75d9ca0480fc609bd8f09bc61efc0f17d0709d
A Hettinger <oninoshiko@gmail.com>
2022-02-11 01:08:33 -0600

    uwsgi: replace build()

--- build/uwsgi/build.sh
+++ build/uwsgi/build.sh
@@ -21,14 +21,27 @@ VER=2.0.20
 PKG=ooce/server/uwsgi
 SUMMARY="fast, self-healing application container server"
 DESC="uwsgi - $SUMMARY"
-ARCHIVE_TYPES="tar.gz"
 
 [ $RELVER -lt 151030 ] && exit 0
 
 #https://projects.unbit.it/downloads/uwsgi-2.0.20.tar.gz
+ARCHIVE_TYPES="tar.gz"
 set_mirror "https://projects.unbit.it"
-#set_chechsum "none"
+set_chechsum "none"
+
 set_arch 64
+configure64() { : }
+
+build() {
+  python uwsgiconfig.py --build core
+  python uwsgiconfig.py --plugin plugins/python core
+  python uwsgiconfig.py --plugin plugins/psgi core
+  python uwsgiconfig.py --plugin plugins/php core
+  python uwsgiconfig.py --plugin plugins/http core
+  python uwsgiconfig.py --plugin plugins/cgi core
+  python uwsgiconfig.py --plugin plugins/syslog core
+  python uwsgiconfig.py --plugin plugins/rsyslog core
+}
 
 init
 download_source "downloads" $PROG $VER
