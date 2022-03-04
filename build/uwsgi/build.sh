a188185cdf6b09643f33c76a9cc0ef7e1e64df1c
A Hettinger <oninoshiko@gmail.com>
2022-03-03 23:11:25 -0600

    Add manifest

--- build/uwsgi/build.sh
+++ build/uwsgi/build.sh
@@ -85,6 +85,7 @@ for file in $DESTDIR/$PREFIX/lib/*.so; do
   manifest_finalise $TMPDIR/manifest.$PROG-$basename $OPREFIX
   PKG=$PKG-$basename RUN_DEPENDS_IPS="ooce/server/uwsgi" make_package -seed $TMPDIR/manifest.$PROG-$basename
 done
+install_smf ooce server-uwsgi.xml
 manifest_uniq $TMPDIR/manifest.$PROG-core $TMPDIR/manifest.uwsgi-*
 manifest_finalise $TMPDIR/manifest.$PROG-core $OPREFIX etc
 make_package -seed $TMPDIR/manifest.$PROG-core core.mog
